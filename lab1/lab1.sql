ed1a424cec55842cf91e6b1ea23a28a57d4c0b54
Daniil Makhov <mahovdnl@gmail.com>
2026-03-17 11:49:02 +0300

    lab 1 upd

--- lab1/lab1.sql
+++ lab1/lab1.sql
@@ -2,7 +2,8 @@
 create table if not exists users (
     id SERIAL primary key,
     username VARCHAR(50) not null unique,
-    email VARCHAR(100) not null unique
+    email VARCHAR(100) not null unique,
+    budget DECIMAL(10,2) not null check (budget > 0)
 );
 
 create table if not exists categories (
@@ -42,19 +43,4 @@ create table if not exists goals (
         on delete cascade
         on update cascade,
     constraint unique_user_goal UNIQUE (user_id, name)
-);
-
-create table if not exists budgets (
-    id SERIAL primary key,
-    user_id INTEGER not null,
-    category_id INTEGER not null,
-    month VARCHAR(10) not null, 
-    amount_limit DECIMAL(10,2) not null check (amount_limit > 0),
-    foreign key (user_id) references users(id)
-        on delete cascade
-        on update cascade,
-    foreign key (category_id) references categories(id)
-        on delete cascade
-        on update cascade,
-    constraint unique_budget UNIQUE (user_id, category_id, month)
 );
